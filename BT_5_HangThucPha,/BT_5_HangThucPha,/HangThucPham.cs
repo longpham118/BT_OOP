d2af9bce5282fd525ff9a0ce2b806440a1d83b63
longpham118 <longp430@gmail.com>
2022-05-31 14:45:05 +0700

    third commit

---
+++ BT_5_HangThucPha,/BT_5_HangThucPha,/HangThucPham.cs
@@ -0,0 +1,129 @@
+﻿namespace BT_5
+{
+    class HangThucPham
+    {
+        private string mahang;
+        private string tenhang;
+        private double dongia;
+        private DateTime ngaysanxuat;
+        private DateTime ngayhethan;
+
+        //Constructor ma hang
+        public HangThucPham(string mahang)
+        {
+            this.mahang = mahang;
+        }
+
+        //Constructor day du tham so
+        public HangThucPham(string mahang, string tenhang, double dongia, DateTime ngaysanxuat, DateTime ngayhethan)
+        { 
+
+            this.mahang = mahang;
+            this.tenhang = tenhang;
+            this.dongia = dongia;
+            this.ngaysanxuat = ngaysanxuat;
+            this.ngayhethan = ngayhethan;
+        }
+
+        public string getMahang()
+        {
+            return mahang;
+        }
+
+        public void setMahang(string mahang)
+        {
+            if (this.mahang == "")
+            {
+                Console.WriteLine("Loi, ma hang rong!");
+            }
+            else
+            {
+                this.mahang = mahang;
+            }
+        }
+
+        public string getTenhang()
+        {
+            return tenhang;
+        }
+
+        public void setTenhang(string tenhang)
+        {
+            if (this.tenhang == "")
+            {
+                this.tenhang = "xxx";
+            }
+            else
+            {
+                this.tenhang = tenhang;
+            }
+        }
+
+        public double getDongia()
+        {
+            return dongia;
+        }
+
+        public void setDongia(double dongia)
+        {
+            if (this.dongia < 0)
+            {
+                this.dongia = 0;
+            }
+            else
+            {
+                this.dongia = dongia;
+            }
+        }
+
+        public DateTime getNgaysanxuat()
+        {
+            return ngaysanxuat;
+        }
+
+        public void setNgaysanxuat(DateTime ngaysanxuat)
+        {
+            int result = DateTime.Compare(this.ngaysanxuat, DateTime.Now);
+            if (result > 0)
+            {
+                this.ngaysanxuat = DateTime.Now;
+            }
+            else
+            {
+                this.ngaysanxuat = ngaysanxuat;
+            }
+        }
+
+        public DateTime getNgayhethan()
+        {
+            return ngayhethan;
+        }
+
+        public void setNgayhethan(DateTime ngayhethan)
+        {
+            int result = DateTime.Compare(this.ngayhethan, this.ngaysanxuat);
+            if (result < 0)
+            {
+                this.ngayhethan = this.ngaysanxuat;
+            }
+            else
+            {
+                this.ngayhethan = ngayhethan;
+            }
+        }
+
+        public string checkHetHan()
+        {
+            int result = DateTime.Compare(this.ngayhethan, DateTime.Now);
+            return result < 0 ? "Hang het han" : "";
+        }
+
+        public string toString()
+        {
+            return String.Format("{0,10} {1,10} {2,20:#,##0.00VND} {3,20} {4,20} {5,20}\n", this.mahang, this.tenhang, this.dongia, this.ngaysanxuat.ToString("dd/MM/yyyy"), this.ngayhethan.ToString("dd/MM/yyyy"), this.checkHetHan());
+        }
+    }
+}
+
+        
+    
---
+++ BT_5_HangThucPha,/BT_5_HangThucPha,/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BT_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HangThucPham tp1 = new HangThucPham("001", "Gao", 100000, new DateTime(2018, 7, 10), new DateTime(2018, 7, 9));
+            HangThucPham tp2 = new HangThucPham("002", "Mi", 5000, new DateTime(2018, 3, 1), new DateTime(2022, 9, 1));
+            HangThucPham tp3 = new HangThucPham("003", "Nuoc", 10000, new DateTime(2017, 3, 1), new DateTime(2022, 3, 1));
+            Console.WriteLine("{0,10} {1,10} {2,20:#,##0.00} {3,20} {4,20} {5,20}\n", "Ma_Hang", "Ten_Hang", "Don_Gia", "Ngay_San_Xuat", "Ngay_Het_Han", "Ghi_Chu");
+            Console.WriteLine(tp1.toString());
+            Console.WriteLine(tp2.toString());
+            Console.WriteLine(tp3.toString());
+        }
+    }
+}
